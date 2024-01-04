c1ef28fb50ec5929933d6d3d0869b9d4ad628814
Guttenberg Junior <ghuttojunior@gmail.com>
2024-01-04 00:14:21 -0300

    homePage update

--- script.js
+++ script.js
@@ -4,40 +4,40 @@ password = document.getElementById("password");
 confirmPassword = document.getElementById("confimPassword");
 
 
-function authentication(){
+function authentication() {
 
 
-    function nameVerify(){
+    function nameVerify() {
         nomeValue = nome.value;
         localStorage.setItem('valueText', nomeValue);
-    
-        if(nomeValue == ""){
+
+        if (nomeValue == "") {
             return false
-        } else{
+        } else {
             return true
         }
     }
 
-    function passwordVerify(){
+    function passwordVerify() {
         verifyOne = password.value;
         verifyTwo = confirmPassword.value;
-    
-        if(verifyOne == verifyTwo){
+
+        if (verifyOne == verifyTwo) {
             return true
-        } else{
+        } else {
             return false
         }
     }
 
 
-    if(nameVerify() && passwordVerify() == true){
+    if (nameVerify() && passwordVerify() == true) {
         window.location.href = "homepage.html";
         alert("Conta criada com sucesso!")
-    } else if(nameVerify() == false && passwordVerify() == true){
+    } else if (nameVerify() == false && passwordVerify() == true) {
         alert("Campo 'nome' preenchido incorretamente")
-    } else if(nameVerify() == true && passwordVerify() == false) {
-         alert("Campo 'senha' preenchido incorretamente")
-    } else{
+    } else if (nameVerify() == true && passwordVerify() == false) {
+        alert("Campo 'senha' preenchido incorretamente")
+    } else {
         alert("ERROR 404")
     }
 }
@@ -47,6 +47,36 @@ function authentication(){
 let nameDisplay = document.getElementById("userName");
 nameDisplay.textContent = localStorage.getItem('valueText');
 //End of name display
+
+
+// Mobile nav display
+
+var navStatus = document.querySelector(".list");
+var mobileMenu = document.querySelector(".menu");
+
+mobileMenu.addEventListener("click", () =>{
+    var nav = document.querySelector(".list")
+    
+    if(nav.style.display === "flex"){   
+        nav.style.display = "none"
+    } else{
+        nav.style.display = "flex"
+    }
+
+})
+//End of mobile Nav
+
+
+
+
+
+
+
+
+
+
+
+// End of mobile nav
 
 
 
